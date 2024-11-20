005be8cdfeca9aba4a49cc111bcb7f77339fcb78
beeehappyandfree <beeehappyandfree@gmail.com>
2024-11-20 12:03:13 +0800

    Add cache implementation

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,21 +1,20 @@
 import { db } from "@/drizzle/db";
 import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
-import { productDetailsSchema } from "@/schemas/product";
+import { CACHE_TAGS, getUserTag , dbCache, revalidateDbCache} from "@/lib/cache";
 import { and, eq } from "drizzle-orm";
 
 export async function getProducts(userId: string, { limit }: { limit: number }) {
-    return db.query.ProductTable.findMany({
-        where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
-        orderBy: ({ createdAt }, { desc }) => desc(createdAt),
-        limit, 
-    })
+    const cacheFn = dbCache(getProductsInternal, { 
+        tags: [getUserTag(userId, CACHE_TAGS.products)]
+     })
+    return cacheFn(userId, { limit })
 }
 
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
     const [newProduct] = await db
         .insert(ProductTable)
         .values(data)
-        .returning({ id: ProductTable.id})
+        .returning({ id: ProductTable.id, userId: ProductTable.clerkUserId })
     try {
         await db.insert(ProductCustomizationTable).values({
             productId: newProduct.id,
@@ -25,6 +24,13 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
     } catch (error) {
         await db.delete(ProductTable).where(eq(ProductTable.id, newProduct.id));
     }
+
+    revalidateDbCache({
+        tag: CACHE_TAGS.products,
+        userId: newProduct.userId,
+        id: newProduct.id
+    })
+
     return newProduct;
 }
 
@@ -32,5 +38,22 @@ export async function deleteProduct({ id, userId }: { id: string, userId: string
     const { rowCount } = await db
         .delete(ProductTable)
         .where(and(eq(ProductTable.id, id), eq(ProductTable.clerkUserId, userId)))
+    
+    if (rowCount > 0) {
+        revalidateDbCache({
+            tag: CACHE_TAGS.products,
+            userId,
+            id,
+        })
+    }
+
     return rowCount > 0
+}
+
+function getProductsInternal(userId: string, { limit }: { limit: number }) {
+    return db.query.ProductTable.findMany({
+        where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+        orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+        limit, 
+    })
 }
--- src/server/db/subscription.ts
+++ src/server/db/subscription.ts
@@ -1,6 +1,5 @@
 import { db } from "@/drizzle/db";
 import { UserSubscriptionTable } from "@/drizzle/schema";
-import { eq } from "drizzle-orm";
 
 export async function createUserSubcription(data: typeof UserSubscriptionTable.$inferInsert) {
     return db.insert(UserSubscriptionTable).values(data)
